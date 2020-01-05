11526b756b24d7e09cec9151c05408210ebaf47e
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-05 12:13:32 +0530

    get set method

---
+++ getset.py
@@ -0,0 +1,14 @@
+class stud:
+    def setname(self,name):
+        self.name = name
+    def getname(self):
+        return self.name
+    n = int(input("enter number of student")
+    for s in range(n):
+            s=stud()
+            print(s)
+            name=input("enter name ")
+            s.setname(name)
+    print(s.getname())
+    print(-------)
+            
