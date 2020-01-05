fb4cf357ed7f608ac9ed7901a977440cf2197a69
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-05 12:05:46 +0530

    addition of 2 numbers in class

---
+++ addition.py
@@ -0,0 +1,9 @@
+class m:
+    def __init__(self,n1,n2):
+        self.no1=n1
+        self.no2=n2
+    def add(self):
+        a=self.no1+self.no2
+        print("addition is ",a)
+ob=m(5,5)
+ob.add()
