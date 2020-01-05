fed7556998cb8885a96437871634a2e85611008b
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-05 12:06:20 +0530

    array

---
+++ array.py
@@ -0,0 +1,14 @@
+arr=["abc","jkl","mno"]
+print(arr)
+print(arr[0])
+for x in arr:
+    print(x)
+arr.append("Honda")
+arr.append("xyz")
+print(arr)
+arr.append("jkl")
+print(arr)
+arr.pop(1)
+print(arr)
+arr.remove("abc")
+print(arr)
