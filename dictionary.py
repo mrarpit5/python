a01b152c8dbe2396f74f37c86599a17cc5bfe8e6
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-05 12:11:36 +0530

    dictonary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dic={"no":"1",
+     "name":"abc"}
+print(dic)
+a=input("enter string ")
+print(a)
+
+n=int(input("enter no "))
+print(n)
---
+++ dictonary.py
@@ -0,0 +1,12 @@
+dic = {"fruite":"apple","sak":"bataka","sweet":"jambu"}
+print(dic)
+
+for i in  dic:
+    print("index is ",i)
+for i in dic:
+    print("elements are ",dic[i])
+print("all items are-->",dic.items())
+print("len is ",len(dic))
+dic["l"]="rabdi"
+print(dic)
+
