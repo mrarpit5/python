1fb4a2769cfd6604fa6198e7b2de2ebab15ee9fa
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-05 12:12:54 +0530

    function demo

---
+++ function.py
@@ -0,0 +1,22 @@
+def fun():
+    print("this is first function")
+fun()
+
+def fa(ar):
+    print("name is "+ar)
+fa("arpit")
+
+def af(fname,lname):
+    print("fname is "+ fname + "last name is" + lname)
+af("abc","xyz")
+
+
+def p(f):
+    for x in f:
+        print(x)
+fruits=["a","b","c"]
+p(fruits)
+
+def cal(n):
+    return n*n
+print(cal(3))
